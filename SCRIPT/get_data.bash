bd1061b90c3d8e3967f1ebe31f456591faed3e79
oceandie <diego.bruciaferri@metoffice.gov.uk>
2025-06-09 14:31:29 +0100

    bugfix

--- SCRIPT/get_data.bash
+++ SCRIPT/get_data.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 #SBATCH --mem=100G
-#SBATCH --time=120
+#SBATCH --time=360
 #SBATCH --ntasks=1
 
 sleep 30
@@ -47,25 +47,27 @@ for MFILE in ${FILE_LST}; do
 #      if [[ $SIZEMASS -ne $SIZESYST ]]; then echo " $FILE is corrupted "; rm $FILE; fi
       if [[ $TIME -eq 0 ]]; then echo " $FILE is corrupted "; rm $FILE; fi
 
-      Tv10=$(ncdump -h $FILE | grep float | grep thetao[_\ ]con | cut -d' ' -f2 | cut -d'(' -f1 )
-      Sv10=$(ncdump -h $FILE | grep float | grep so[_\ ]abs | cut -d' ' -f2 | cut -d'(' -f1 )
-      Tv80=$(ncdump -h $FILE | grep float | grep thetao[_\ ]pot | cut -d' ' -f2 | cut -d'(' -f1 )
-      Sv80=$(ncdump -h $FILE | grep float | grep so[_\ ]pra | cut -d' ' -f2 | cut -d'(' -f1 )
-      #echo "Tv10= " $Tv10 ", Sv10= " $Sv10
-      #echo "Tv80= " $Tv80 ", Sv80= " $Sv80
-      if [[ "$Tv10" == "thetao_con" && "$Sv10" == "so_abs" ]]; then TEOS10=1; fi
-      if [[ "$Tv80" == "thetao_pot" && "$Sv80" == "so_pra" ]]; then EOS80=1; fi
-      echo "TEOS10= " $TEOS10 ", EOS80= " $EOS80
+      if [[ "${GRID}" == "T" ]]; then
+         Tv10=$(ncdump -h $FILE | grep float | grep thetao[_\ ]con | cut -d' ' -f2 | cut -d'(' -f1 )
+         Sv10=$(ncdump -h $FILE | grep float | grep so[_\ ]abs | cut -d' ' -f2 | cut -d'(' -f1 )
+         Tv80=$(ncdump -h $FILE | grep float | grep thetao[_\ ]pot | cut -d' ' -f2 | cut -d'(' -f1 )
+         Sv80=$(ncdump -h $FILE | grep float | grep so[_\ ]pra | cut -d' ' -f2 | cut -d'(' -f1 )
+         #echo "Tv10= " $Tv10 ", Sv10= " $Sv10
+         #echo "Tv80= " $Tv80 ", Sv80= " $Sv80
+         if [[ "$Tv10" == "thetao_con" && "$Sv10" == "so_abs" ]]; then TEOS10=1; fi
+         if [[ "$Tv80" == "thetao_pot" && "$Sv80" == "so_pra" ]]; then EOS80=1; fi
+         echo "TEOS10= " $TEOS10 ", EOS80= " $EOS80
 
-      if [[ "$TEOS10" == 0 ]]; then
-         # If file has only one of thetao_con or so_abs, something has gone wrong
-         # delete and restore from MASS again. 
-         echo " $FILE is corrupted "; rm $FILE
-      elif [[ "$TEOS10" == 1 && "$EOS80" == 0 ]]; then
-         # If file has only TEOS10 variables, we need to compute the EOS80 ones. 
-         CONVERT_EOS_LIST="$CONVERT_EOS_LIST $FILE"
-      else
-         echo 'nothing to do ... '
+         if [[ "$TEOS10" == 0 ]]; then
+            # If file has only one of thetao_con or so_abs, something has gone wrong
+            # delete and restore from MASS again. 
+            echo " $FILE is corrupted "; rm $FILE
+         elif [[ "$TEOS10" == 1 && "$EOS80" == 0 ]]; then
+            # If file has only TEOS10 variables, we need to compute the EOS80 ones. 
+            CONVERT_EOS_LIST="$CONVERT_EOS_LIST $FILE"
+         else
+            echo 'nothing to do ... '
+         fi
       fi
    fi
 
@@ -83,13 +85,15 @@ if [[ -n "$MOO_GET_LIST" ]];then
 fi
 
 for FILE in $MOO_RESTORED_LIST;do
-   Tv10=$(ncdump -h $FILE | grep float | grep thetao[_\ ]c | cut -d' ' -f2 | cut -d'(' -f1 )
-   Sv10=$(ncdump -h $FILE | grep float | grep so[_\ ]a | cut -d' ' -f2 | cut -d'(' -f1 )
-   echo "$FILE : $Tv10 $Sv10"
-   if [[ "$Tv10" == "thetao_con" && "$Sv10" == "so_abs" ]]; then 
-      echo "hello!"
-      CONVERT_EOS_LIST="$CONVERT_EOS_LIST $FILE"
-   fi
+   if [[ "${GRID}" == "T" ]]; then   
+      Tv10=$(ncdump -h $FILE | grep float | grep thetao[_\ ]c | cut -d' ' -f2 | cut -d'(' -f1 )
+      Sv10=$(ncdump -h $FILE | grep float | grep so[_\ ]a | cut -d' ' -f2 | cut -d'(' -f1 )
+      echo "$FILE : $Tv10 $Sv10"
+      if [[ "$Tv10" == "thetao_con" && "$Sv10" == "so_abs" ]]; then 
+         echo "hello!"
+         CONVERT_EOS_LIST="$CONVERT_EOS_LIST $FILE"
+      fi
+   fi 
    # Set standard_name for depth coordinate so Iris will recognise it:
    [[ "$FILE" =~ *grid-T\.nc ]] && depvar="deptht"
    [[ "$FILE" =~ *grid-U\.nc ]] && depvar="depthu"
