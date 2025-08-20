a48e78552006f50435c1657c45c8c6fb19ebb545
Hatim Chahout <hatim.chahout@users.noreply.github.com>
2025-08-20 10:52:05 +0100

    Amended test setup script to create param.bash from scratch. #66

--- tests/run_test_setup.bash
+++ tests/run_test_setup.bash
@@ -4,29 +4,36 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Mock the 'sbatch' command to write to stdout
-sbatch() {
-  echo "MOCK SBATCH: $@"
-}
-export -f sbatch
-
-# Mock other commands used in the run_proc.bash
+# Mock functions from run_proc.bash
+sbatch() { echo "MOCK SBATCH: $@"; }
 slurm_wait() { :; }
 moo_wait() { :; }
 sacct() { echo ""; }
-squeue() { echo "P$$"; } # Add this new mock function
-export -f slurm_wait moo_wait sacct squeue
+squeue() { echo "P$$"; }
+export -f sbatch slurm_wait moo_wait sacct squeue
 
 # Function to set up the temporary environment for a test
 function setup_test_env() {
   export TMP_DIR=$(mktemp -d)
-  cp param.bash "$TMP_DIR/param.bash"
+  
+  # Create an empty param.bash and add the mock paths to it
+  touch "$TMP_DIR/param.bash"
+  echo "export MARINE_VAL=$TMP_DIR" >> "$TMP_DIR/param.bash"
+  echo "export MSKPATH=$TMP_DIR/MESH_MASK_DIR" >> "$TMP_DIR/param.bash"
+  echo "export CDFPATH=$TMP_DIR/CDFTOOLS_DIR/bin" >> "$TMP_DIR/param.bash"
+  echo "export NMLPATH=$TMP_DIR/nam_cdf_names" >> "$TMP_DIR/param.bash"
+  echo "export OBSPATH=$TMP_DIR/OBS_PATH_DIR" >> "$TMP_DIR/param.bash"
+  
+  # Create the necessary directories and files
   mkdir -p "$TMP_DIR/CDFTOOLS_DIR/bin"
   mkdir -p "$TMP_DIR/MESH_MASK_DIR"
+  mkdir -p "$TMP_DIR/OBS_PATH_DIR"
   touch "$TMP_DIR/CDFTOOLS_DIR/bin/cdf_tool"
   touch "$TMP_DIR/MESH_MASK_DIR/mesh_mask.nc"
   touch "$TMP_DIR/MESH_MASK_DIR/bathy.nc"
   touch "$TMP_DIR/nam_cdf_names"
+  touch "$TMP_DIR/OBS_PATH_DIR/your_obs_abs_sal_file.nc"
+  touch "$TMP_DIR/OBS_PATH_DIR/your_obs_con_tem_file.nc"
 }
 
 # Function to clean up the temporary environment after a test
