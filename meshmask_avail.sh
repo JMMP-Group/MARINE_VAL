9e9513c244cec55e34c263357f4f8cddba0a8187
Dave Storkey <dave.storkey@metoffice.gov.uk>
2026-09-03 12:37:18 +0100

    1. Add bathy files list in meshmask_avail.sh. 2. Update get_data.bash to only check for standard_name attribute.

--- meshmask_avail.sh
+++ meshmask_avail.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 #
 # List available mesh_mask files based on the setting of MSKPATH in param.bash
@@ -5,9 +6,17 @@
 
 MSKPATH=$(grep "^export.*MSKPATH" param.bash | cut -d"=" -f2)
 echo "MSKPATH : $MSKPATH"
-echo "Available mesh_mask files:"
+echo ""
+echo "Available mesh_mask (*mesh*) files:"
 mesh_files=$(ls $MSKPATH | grep "mesh")
 for file in $mesh_files
+do
+    echo $file
+done
+echo ""
+echo "Available bathymetry (*bathy*) files:"
+bathy_files=$(ls $MSKPATH | grep "bathy")
+for file in $bathy_files
 do
     echo $file
 done
